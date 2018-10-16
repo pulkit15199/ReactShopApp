8742dabc0eded7cf9c2e7e620edd6704bd0d7fa7
pulkit15199 <pulkit15199@gmail.com>
2018-10-16 11:28:29 +0530

    Updates

---
+++ precache-manifest.9ada642a6dd6d8d025348e7ebba9cabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f221c9848c8453ca8f9b",
+    "url": "/ReactShopApp/static/js/runtime~main.f221c984.js"
+  },
+  {
+    "revision": "cee2147a41785306bc33",
+    "url": "/ReactShopApp/static/js/main.cee2147a.chunk.js"
+  },
+  {
+    "revision": "7e17cbc998b4b32adcdb",
+    "url": "/ReactShopApp/static/js/1.7e17cbc9.chunk.js"
+  },
+  {
+    "revision": "cee2147a41785306bc33",
+    "url": "/ReactShopApp/static/css/main.c4578fa6.chunk.css"
+  },
+  {
+    "revision": "7e17cbc998b4b32adcdb",
+    "url": "/ReactShopApp/static/css/1.90d44d13.chunk.css"
+  },
+  {
+    "revision": "d06b705bc8d2948e9666049ed464b1b1",
+    "url": "/ReactShopApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b40d9ab4dce0b09034fb4da0761b92e.js"
+  "/ReactShopApp/precache-manifest.9ada642a6dd6d8d025348e7ebba9cabe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactShopApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
