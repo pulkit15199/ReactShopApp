9c351e30c18e179dba395283e7c5500b9fc817c1
pulkit15199 <pulkit15199@gmail.com>
2018-10-16 11:25:17 +0530

    Updates

---
+++ precache-manifest.7b40d9ab4dce0b09034fb4da0761b92e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f6b348c821e94585348",
+    "url": "/static/js/main.4f6b348c.chunk.js"
+  },
+  {
+    "revision": "0c6f78779b1cf2e48027",
+    "url": "/static/js/1.0c6f7877.chunk.js"
+  },
+  {
+    "revision": "4f6b348c821e94585348",
+    "url": "/static/css/main.c4578fa6.chunk.css"
+  },
+  {
+    "revision": "0c6f78779b1cf2e48027",
+    "url": "/static/css/1.90d44d13.chunk.css"
+  },
+  {
+    "revision": "413fd789a9b52da04cc63fe7f9f42e3f",
+    "url": "/index.html"
+  }
+];
